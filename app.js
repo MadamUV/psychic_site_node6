a8f08bc600d91b13a1566ec5e42fe53992fa787c
Erin M. Masterson <erin.margot.masterson@gmail.com>
2017-10-25 15:50:03 -0400

    re-added lots of svgs and static assets

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+var bodyParser = require('body-parser');
 
 app.get('/', (req, res) => {
 	res.sendFile('/human2.html', { root: __dirname });
+});
+
+app.use(express.static((__dirname + '/static')));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(bodyParser.json());
+app.post("/clothes", function(req, res){
+	res.sendFile("/"+req.body.clothing, { root: __dirname });
 });
 
 app.listen(process.env.PORT || 3000);
