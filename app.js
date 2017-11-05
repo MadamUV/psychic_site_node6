bdce0e38d2be6ec78e7ea14196b3857d5805c82e
Erin M. Masterson <erin.margot.masterson@gmail.com>
2017-11-05 18:34:22 -0500

    first commit

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+var io = require('socket.io');
+var verbose = false;
+var UUID = require('node-uuid');
 var bodyParser = require('body-parser');
 
 app.get('/', (req, res) => {
 	res.sendFile('/human2.html', { root: __dirname });
+});
+//just use a different route
+app.get("/world", function(req, res, next){
+	var file = req.params[0];
+	if(verbose) console.log('express requested');
+	res.sendFile(__dirname + "/" + file);
 });
 
 app.use(express.static((__dirname + '/static')));
@@ -15,5 +24,9 @@ app.use(bodyParser.json());
 app.post("/clothes", function(req, res){
 	res.sendFile("/"+req.body.clothing, { root: __dirname });
 });
+app.post("/world", function(req, res){
+	res.sendFile("/"+req.body.world, { root: __dirname });
+});
+
 
 app.listen(process.env.PORT || 3000);
